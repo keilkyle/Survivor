2618f20ec13fef2835845e167b55387bf836881d
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-04-27 18:16:21 -0700

    got the initial fetch working and the database initialized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+  
+  const [castaways, maroooning] = useState([])
+
+  useEffect(()=> {
+    fetch("http://localhost:3000/castaways")
+    .then(resp => resp.json())
+    .then(data => maroooning(data))
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
         <p>
-         Welcome to... survivor!
+         Welcome to... Survivor!
         </p>
+        {castaways.map((castaway) => <div key={castaway.id}><h3>{castaway.name}</h3><img src={castaway.image} /></div>)}
       </header>
     </div>
   );
